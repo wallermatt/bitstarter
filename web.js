1cb4a9b6dc5f0a7b6bc0a5b44aeba371dae7ba76
Matthew Waller <waller_matthew@yahoo.co.uk>
2013-07-07 12:19:17 +0000

    Index file and changes

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync('index.html', 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
